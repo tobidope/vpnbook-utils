df7a4185869d498e8699d393d9016b3e35692625
Tobias Bell <tobias.bell@gmail.com>
2014-01-27 19:40:42 +0100

    Fixed error caused by not enclosing expansions with ""

--- test/test_vpnbook.sh
+++ test/test_vpnbook.sh
@@ -10,12 +10,12 @@ get_test_data() {
 }
 
 test_extract_credentials() {
-	local result=$(get_test_data | extract_credentials)
-	local expected=$(cat <<-EOF
+	local result="$(get_test_data | extract_credentials)"
+	local expected="$(cat <<-EOF
 		vpnbook
 		qe5Egawr
 		EOF
-	)
+	)"
 	assertEquals "$expected" "$result"
 
 	echo "Wrong input" | extract_credentials
@@ -23,14 +23,14 @@ test_extract_credentials() {
 }
 
 test_extract_config_urls() {
-	local result=$(get_test_data | extract_config_urls)
-	local expected=$(cat <<-EOF
+	local result="$(get_test_data | extract_config_urls)"
+	local expected="$(cat <<-EOF
 	http://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-Euro1.zip
 	http://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-Euro2.zip
 	http://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-UK1.zip
 	http://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-US1.zip
 	EOF
-	)
+	)"
 	assertEquals "$expected" "$result"
 }
 
